9a4aaa99d63962e747509f65cb03831c61898b60
Ingvar Drikkit <ingvar.drikkit@gmail.com>
2021-03-25 13:52:04 +0200

    Songs folder + more testing

--- src/mp3Test.java
+++ src/mp3Test.java
@@ -2,11 +2,18 @@ import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
+import java.io.File;
 import java.io.IOException;
 
 public class mp3Test {
     public static void main(String[] args) throws InvalidDataException, IOException, UnsupportedTagException {
-        Mp3File mp3file = new Mp3File("src/jama.mp3");
-        System.out.println(mp3file.getId3v1Tag().getArtist());
+
+        File folder = new File("songs/");
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles){
+            Mp3File mp3file = new Mp3File(file.getCanonicalPath());
+            System.out.println(mp3file.getId3v1Tag().getTitle() + " - " + mp3file.getId3v1Tag().getArtist());
+        }
     }
 }
