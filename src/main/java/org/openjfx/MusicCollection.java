492a81cf26e0542df42492300d9bd96ccdb875ce
Ingvar Drikkit <ingvar.drikkit@gmail.com>
2021-04-03 23:03:26 +0300

    removed obsolete field

--- src/main/java/org/openjfx/MusicCollection.java
+++ src/main/java/org/openjfx/MusicCollection.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 
 public class MusicCollection {
     private ArrayList<Song> songList = new ArrayList<>();
-    private ArrayList<MediaPlayer> songListMediaPlayer = new ArrayList<>();
 
     public ArrayList<Song> getSongList() {
         return songList;
